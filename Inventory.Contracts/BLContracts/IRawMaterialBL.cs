874cd2943d400403350e9e147da29826e0217d55
RohitKumarCG <54537801+RohitKumarCG@users.noreply.github.com>
2019-09-25 03:30:33 +0530

    Update IRawMaterialBL.cs

--- Inventory.Contracts/BLContracts/IRawMaterialBL.cs
+++ Inventory.Contracts/BLContracts/IRawMaterialBL.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Capgemini.Inventory.Entities;
 
 namespace Capgemini.Inventory.Contracts.BLContracts
 {
-    public class IRawMaterialBL : IDisposable
+    public interface IRawMaterialBL : IDisposable
     {
+        Task<bool> AddRawMaterialBL(RawMaterial newRawMaterial);
+        Task<List<RawMaterial>> GetAllRawMaterialsBL();
+        Task<RawMaterial> GetRawMaterialByRawMaterialIDBL(Guid searchRawMaterialID);
+        Task<RawMaterial> GetRawMaterialByRawMaterialNameBL(string searchRawMaterialName);
+        Task<RawMaterial> GetRawMaterialByRawMaterialCodeBL(string searchRawMaterialCode);
+        Task<bool> UpdateRawMaterialBL(RawMaterial updateRawMaterial);
+        Task<bool> DeleteRawMaterialBL(Guid deleteRawMaterialID);
     }
 }
