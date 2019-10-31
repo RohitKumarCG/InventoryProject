9999de95324a6e039ae91c24706e285438c7ee7c
RohitKumarCG <54537801+RohitKumarCG@users.noreply.github.com>
2019-10-31 14:36:30 +0530

    Add files via upload

---
+++ MVC/Models/ProductViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Mvc.Models
+{
+    // Developed by Rohit Kumar (Group 4)
+    // Creation Date : 2019/10/24
+    // Last Modified Date : 
+
+    public class ProductViewModel
+    {
+        //Properties
+        public System.Guid ProductID { get; set; }
+
+        [Required(ErrorMessage ="Product Name can't be blank")]
+        [RegularExpression("^[A-Za-z ]*$", ErrorMessage ="Product Name should contain alphabets only" )]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Product Code can't be blank")]
+        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Product Code should contain alphabets only without spaces")]
+        public string ProductCode { get; set; }
+
+        public string ProductType { get; set; }
+
+        [Required(ErrorMessage = "Price can't be blank")]
+        [Range(0.99, 100000, ErrorMessage = "Price should be between 0.99 and 100000")]
+        public double? ProductUnitPrice { get; set; }
+    }
+}
---
+++ MVC/Models/RawMaterialViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Mvc.Models
+{
+    // Developed by Rohit Kumar (Group 4)
+    // Creation Date : 2019/10/24
+    // Last Modified Date : 2019/10/25
+
+    public class RawMaterialViewModel
+    {
+        //Properties
+        public System.Guid RawMaterialID { get; set; }
+
+        [Required(ErrorMessage = "Raw Material Name can't be blank")]
+        [RegularExpression("^[A-Za-z ]*$", ErrorMessage = "Raw Naterial Name should contain alphabets only")]
+        public string RawMaterialName { get; set; }
+
+        [Required(ErrorMessage = "Raw Material Code can't be blank")]
+        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Raw Material Code should contain alphabets only without spaces")]
+        public string RawMaterialCode { get; set; }
+        
+        [Required(ErrorMessage = "Price can't be blank")]
+        [Range(0.99, 100000, ErrorMessage ="Price should be between 0.99 and 100000")]
+        public decimal? RawMaterialUnitPrice { get; set; }
+    }
+}
