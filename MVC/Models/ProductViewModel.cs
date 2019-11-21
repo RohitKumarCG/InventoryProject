5a8a3245908a51004c315c29a34ea5a75df19211
RohitKumarCG <54537801+RohitKumarCG@users.noreply.github.com>
2019-11-21 10:45:41 +0530

    Add files via upload

--- MVC/Models/ProductViewModel.cs
+++ MVC/Models/ProductViewModel.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Inventory.Mvc.Models
 {
     // Developed by Rohit Kumar (Group 4)
     // Creation Date : 2019/10/24
-    // Last Modified Date : 
+    // Last Modified Date : 2019/11/20
 
     public class ProductViewModel
     {
         //Properties
         public System.Guid ProductID { get; set; }
 
-        [Required(ErrorMessage ="Product Name can't be blank")]
-        [RegularExpression("^[A-Za-z ]*$", ErrorMessage ="Product Name should contain alphabets only" )]
+        [Required(ErrorMessage = "Product Name can't be blank")]
+        [RegularExpression("^[A-Za-z ]*$", ErrorMessage = "Name should contain alphabets only")]
+        [MinLength(3, ErrorMessage = "Name should contain atleast 3 alphabets")]
+        [MaxLength(30, ErrorMessage = "Name cannot contain more than 30 alphabets")]
         public string ProductName { get; set; }
 
         [Required(ErrorMessage = "Product Code can't be blank")]
-        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Product Code should contain alphabets only without spaces")]
+        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Code should contain alphabets only without spaces")]
+        [MinLength(3, ErrorMessage = "Code should contain atleast 3 alphabets")]
+        [MaxLength(30, ErrorMessage = "Code cannot contain more than 30 alphabets")]
         public string ProductCode { get; set; }
 
         public string ProductType { get; set; }
--- MVC/Models/RawMaterialViewModel.cs
+++ MVC/Models/RawMaterialViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Inventory.Mvc.Models
 {
@@ -16,15 +12,19 @@ namespace Inventory.Mvc.Models
         public System.Guid RawMaterialID { get; set; }
 
         [Required(ErrorMessage = "Raw Material Name can't be blank")]
-        [RegularExpression("^[A-Za-z ]*$", ErrorMessage = "Raw Naterial Name should contain alphabets only")]
+        [RegularExpression("^[A-Za-z ]*$", ErrorMessage = "Name should contain alphabets only")]
+        [MinLength(3, ErrorMessage = "Name should contain atleast 3 alphabets")]
+        [MaxLength(30, ErrorMessage = "Name cannot contain more than 30 alphabets")]
         public string RawMaterialName { get; set; }
 
         [Required(ErrorMessage = "Raw Material Code can't be blank")]
-        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Raw Material Code should contain alphabets only without spaces")]
+        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Code should contain alphabets only without spaces")]
+        [MinLength(3, ErrorMessage = "Code should contain atleast 3 alphabets")]
+        [MaxLength(30, ErrorMessage = "Code cannot contain more than 30 alphabets")]
         public string RawMaterialCode { get; set; }
-        
+
         [Required(ErrorMessage = "Price can't be blank")]
-        [Range(0.99, 100000, ErrorMessage ="Price should be between 0.99 and 100000")]
+        [Range(0.99, 100000, ErrorMessage = "Price should be between 0.99 and 100000")]
         public decimal? RawMaterialUnitPrice { get; set; }
     }
 }
